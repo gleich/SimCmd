1887f680428955ef0a01b55b9bcbcd66702509f9
Matthew Gleich <matthewgleich@gmail.com>
2020-06-19 23:55:22 -0400

    ✨ Create runner
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- main.go
+++ main.go
@@ -18,19 +18,50 @@ func main() {
 
 	config := config.Extract()
 	sectionStatuses := map[string]map[string]section.Status{}
+
+	// Initalizing the section statuses
 	for sectionName, commands := range config {
 		wg.Add(1)
-		sectionsRunning++
-		go section.Initialize(sectionName, commands, sectionStatuses, &mutex, &sectionsRunning, &wg)
+		go section.Initialize(sectionName, commands, sectionStatuses, &mutex, &wg)
 	}
-
 	wg.Wait()
+
+	// Running the sections
+	for sectionName, commands := range config {
+		go section.Run(sectionName, commands, sectionStatuses, &mutex, &sectionsRunning)
+	}
 
 	goterm.Clear()
 	goterm.MoveCursor(1, 1)
-	for sectionName, statuses := range sectionStatuses {
-		goterm.Println(statuses)
-		goterm.Println(goterm.Color(fmt.Sprintf("%v ⋯ %v commands", sectionName, len(statuses)), goterm.GREEN))
+	for sectionsRunning != 0 {
+		for sectionName, statuses := range sectionStatuses {
+			goterm.Println(statuses)
+			goterm.Println(goterm.Color(fmt.Sprintf("%v ⋯ %v commands", sectionName, len(statuses)), goterm.GREEN))
+			commandsOutput := 0
+			for command, status := range statuses {
+				prefix := " ├──"
+				if commandsOutput != len(statuses) {
+					prefix = " └──"
+				}
+				var indicator string
+				var color int
+				switch status {
+				case section.NotStarted:
+					indicator = " "
+					color = goterm.WHITE
+				case section.Started:
+					indicator = "◌"
+					color = goterm.YELLOW
+				case section.Success:
+					indicator = "✔"
+					color = goterm.GREEN
+				case section.Failed:
+					indicator = "✗"
+					color = goterm.RED
+				}
+				goterm.Println(prefix + goterm.Color(indicator, color) + " " + command)
+			}
+		}
 	}
 	goterm.Flush()
 }
--- section/section.go
+++ section/section.go
@@ -1,12 +1,13 @@
 package section
 
 import (
+	"os/exec"
+	"strings"
 	"sync"
 )
 
 // Initialize ... Initialize a section
-func Initialize(sectionName string, commands []string, statuses map[string]map[string]Status, mutex *sync.Mutex, sectionsRunning *int, wg *sync.WaitGroup) {
-	// Setting initial as NotStarted
+func Initialize(sectionName string, commands []string, statuses map[string]map[string]Status, mutex *sync.Mutex, wg *sync.WaitGroup) {
 	mutex.Lock()
 	commandsAdded := 0
 	for _, command := range commands {
@@ -17,7 +18,28 @@ func Initialize(sectionName string, commands []string, statuses map[string]map[s
 		commandsAdded++
 	}
 	mutex.Unlock()
-
-	*sectionsRunning--
 	wg.Done()
+}
+
+// Run ... Run a section
+func Run(sectionName string, commands []string, statuses map[string]map[string]Status, mutex *sync.Mutex, sectionsRunning *int) {
+	*sectionsRunning++
+	for _, command := range commands {
+		chunks := strings.Split(command, " ")
+		cmd := exec.Command(chunks[0], chunks[1:]...)
+		mutex.Lock()
+		statuses[sectionName][command] = Started
+		mutex.Unlock()
+		err := cmd.Run()
+		if err != nil {
+			mutex.Lock()
+			statuses[sectionName][command] = Failed
+			mutex.Unlock()
+		} else {
+			mutex.Lock()
+			statuses[sectionName][command] = Success
+			mutex.Unlock()
+		}
+	}
+	*sectionsRunning--
 }
